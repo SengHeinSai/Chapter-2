e2bd5b4bdba9b7a67d53179682b823b3e4b0702c
EC2 Default User <ec2-user@ip-172-31-29-221.ap-southeast-1.compute.internal>
2021-10-24 06:25:31 +0000

    second commit

--- main.tf
+++ main.tf
@@ -4,8 +4,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "firstec2" {
-  ami           = "ami-0d058fe428540cd89"
-  instance_type = "t2.micro"
-  
-  tags = { Name = "terraform Example"}
+  ami                    = "ami-0d058fe428540cd89"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.firstec2.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busy­box ht­tpd -f -p 8080 &
+              EOF
+
+  tags = { Name = "terraform Example" }
+}
+
+resource "aws_security_group" "firstec2" {
+  name = "terraform-firstec2-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
